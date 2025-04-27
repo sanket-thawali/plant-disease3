9a510a22f78b85b35f5b028a5e633e19fac7292a
sanket-thawali <146487113+sanket-thawali@users.noreply.github.com>
2025-04-27 23:35:18 +0530

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -27,7 +27,7 @@ const HomePage = (props) => {
 
   const handleGetStartedClick = () => {
     if (user) {
-      window.open("https://plant-disease-esbbmdl9nmfc6nvpt9h8ti.streamlit.app/", "_blank");
+      window.open("https://plant-disease-detect-ddncxdyqqzxryamyt72kgm.streamlit.app/", "_blank");
       setShowPopup(false);
     } else {
       setShowPopup(true);
