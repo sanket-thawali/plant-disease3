91a1f18596628f8aa8edcef070d444dbaf9b05e2
sanket-thawali <146487113+sanket-thawali@users.noreply.github.com>
2025-04-28 00:19:11 +0530

    Update useLogin.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -13,7 +13,7 @@ export const useLogin = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch(`http://127.0.0.1:4000/api/user/${role}/login`, {
+    const response = await fetch(`https://plant-disease-detection-ap18.onrender.com/api/user/${role}/login`, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password ,role})
