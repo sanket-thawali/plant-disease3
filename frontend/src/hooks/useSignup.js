2e95400cb6bbfe371808dc02b0b4d7ff39f11e1a
sanket-thawali <146487113+sanket-thawali@users.noreply.github.com>
2025-04-28 00:19:45 +0530

    Update useSignup.js

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -11,7 +11,7 @@ export const useSignup = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch(`http://127.0.0.1:4000/api/user/${role}/signup`, {
+    const response = await fetch(`https://plant-disease-detection-ap18.onrender.com/api/user/${role}/signup`, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password ,role})
